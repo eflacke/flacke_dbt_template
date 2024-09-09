763e6d07aa19b4fa7ecf75afa94a7f90bff4fca1
Eirik Flacké <143072554+eflacke@users.noreply.github.com>
2024-09-09 15:29:17 +0200

    Add files via upload

--- macros/get_columns.sql
+++ macros/get_columns.sql
@@ -1,21 +1,34 @@
-{%- macro get_columns(dbt_relation) %}
-    {% set columns_query %}
-    select 
-        c.name as column_name,
-        t.name as column_type
-    from sys.columns c
-    inner join sys.types t
-        on c.user_type_id = t.user_type_id
-    where c.object_id = object_id('{{ dbt_relation }}')
-    {% endset %}
-    
-    {% set results = run_query(columns_query) %}
+{% macro get_columns(dbt_relation)%}
 
-    {% if execute %}
-        {% set results_list = results.rows %}
-    {% else %}
-        {% set results_list = [] %}
-    {% endif %}
+{%- set drop_query %}
+drop table if exists #tmp_{{dbt_relation.identifier}}_cols;
+{% endset %}
+{%- do run_query(drop_query)%}
 
-    {{ return(results_list) }}
-{%- endmacro %}
+{%- set into_query %}
+select top 0 * into #tmp_{{dbt_relation.identifier}}_cols from {{dbt_relation}};
+{% endset %}
+{%- do run_query(into_query)%}
+
+{% set columns_query %}
+select
+    columns.name as column_name
+    ,types.name as column_type
+from tempdb.sys.columns
+inner join tempdb.sys.types
+    on columns.system_type_id = types.user_type_id
+where object_id = object_id('tempdb.dbo.#tmp_{{dbt_relation.identifier}}_cols');
+{% endset %}
+{% set results = run_query(columns_query) %}
+
+{# execute is a Jinja variable that returns True when dbt is in "execute" mode i.e. True when running dbt run but False during dbt compile. #}
+{% if execute %}
+{# agate.table.rows is agate.MappedSequence in which data that can be accessed either by numeric index or by key. #}
+{% set results_list = results.rows %}
+{% else %}
+{% set results_list = [] %}
+{% endif %}
+
+{{ return(results_list) }}
+
+{% endmacro %}
